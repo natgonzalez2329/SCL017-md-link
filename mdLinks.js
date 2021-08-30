0462a5feb1ab68d78954c2046ba1781700c08a86
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-08-30 12:55:45 -0300

    read folder inside of a folder

--- mdLinks.js
+++ mdLinks.js
@@ -3,63 +3,25 @@ const path = require('path');
 const markdownLinkExtractor = require('markdown-link-extractor');
 const axios = require('axios');
 
-const fileReader = (pathFile) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(pathFile, 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(data);
-        });
-    });
-}
-
-const folderReader = (pathFolder) => {
-    return new Promise((resolve, reject) => {
-        fs.readdir(pathFolder, (error, files) => {
-            if (error) {
-                reject(error);
-            }
-            if (files.length > 0) {
-                const filesMd = files.filter((file) => path.extname(file.toLowerCase()) === '.md');
-                resolve(filesMd);
-            }
-        });
-    });
-}
-
-const linkSearcher = (dataFile, options, filePath) => {
-    return new Promise((resolve, reject) => {
-        try {
-            const links = markdownLinkExtractor(dataFile, true).filter(link => link.href.includes('https://') || link.href.includes('http://'));
-            const linksData = links.map(link => ({ href: link.href, text: link.text, file: filePath }));
-            if (options.validate) {
-                resolve(Promise.all(linksData.map(linkData => {
-                    return axios.get(linkData.href).then((response) => {
-                        linkData.status = response.status;
-                        if (response.status === 200) {
-                            linkData.ok = response.statusText;
-                        }
-                        return linkData;
-                    }).catch(error => {
-                        linkData.status = error.response.status;
-                        linkData.ok = 'fail';
-                        return linkData;
-                    });
-                })));
-            } else {
-                resolve(linksData)
-            };
-        } catch (error) {
-            reject(error)
-        }
-    });
-}
-
 const isFolder = (fullPath) => {
     const pathExists = fs.statSync(fullPath);
     return pathExists && pathExists.isDirectory();
-}
+};
+
+const folderReader = (pathFolder) => {
+  return new Promise((resolve, reject) => {
+      fs.readdir(pathFolder, (error, files) => {
+          if (error) {
+              reject(error);
+          }
+          if (files.length > 0) {
+              const filesMd = files.filter((file) => path.extname(file.toLowerCase()) === '.md');
+              resolve(filesMd);
+          }
+      });
+  });
+};
+
 const isFile = (fullPath) => {
     let result = false;
     if (typeof fullPath === 'object') {
@@ -68,13 +30,62 @@ const isFile = (fullPath) => {
         result = path.extname(fullPath).toLowerCase() === '.md'
     }
     return result;
-}
+};
 
-const mdlinksFinder = (fullPath, options = { validate: false }) => {
+const fileReader = (pathFile) => {
+  return new Promise((resolve, reject) => {
+      fs.readFile(pathFile, 'utf8', (err, data) => {
+          if (err) {
+              reject(err);
+          }
+          resolve(data);
+      });
+  });
+};
+
+const statsLinks = (links) => {
+  const totalLinks = links.length;
+  const uniqueLinks = [...new Set(links.map((allLinks) => allLinks.href))].length;
+  const brokenLinks = links.filter((linkData) => linkData.ok === 'fail').length;
+  const statData = { total: totalLinks, unique: uniqueLinks };
+  if(brokenLinks > 0) {
+    statData['broken'] = brokenLinks;
+  };
+  return statData;
+};
+
+const linkSearcher = (dataFile, options, filePath) => {
+  return new Promise((resolve, reject) => {
+      try {
+          const links = markdownLinkExtractor(dataFile, true).filter(link => link.href.includes('https://') || link.href.includes('http://'));
+          const linksData = links.map(link => ({ href: link.href, text: link.text, file: filePath }));
+          if (options.validate) {
+            resolve(Promise.all(linksData.map(linkData => {
+              return axios.get(linkData.href).then((response) => {
+                  linkData.status = response.status;
+                  if (response.status === 200) {
+                      linkData.ok = response.statusText;
+                  }
+                  return linkData;
+              }).catch(error => {
+                  linkData.status = error.response.status;
+                  linkData.ok = 'fail';
+                  return linkData;
+              });
+          })));
+          } else {
+              resolve(linksData)
+          };
+      } catch (error) {
+          reject(error)
+      }
+  });
+};
+
+const mdlinksFinder = (fullPath, options = { validate: false, stats: false }) => {
     return new Promise((resolve, reject) => {
         try {
             if (isFile(fullPath)) {
-                console.log('its a fileeeeeeeee');
                 let pathArray = [];
                 if (typeof fullPath !== 'object') {
                     pathArray.push(fullPath);
@@ -84,20 +95,32 @@ const mdlinksFinder = (fullPath, options = { validate: false }) => {
                 pathArray.forEach((path) => {
                     fileReader(path, options).then(fileData => {
                         linkSearcher(fileData, options, path).then(linksData => {
+                          if (options.validate && !options.stats) {
+                            resolve(linksData);                           
+                          } 
+                          if(options.stats && !options.validate) {
+                            const stats = statsLinks(linksData);
+                          resolve(stats);
+                          } 
+                          if(options.validate && options.stats) {
+                          const stats = statsLinks(linksData);
+                            resolve(stats);
+                          } 
                             resolve(linksData);
                         }).catch(e => console.log(e));
                     }).catch(e => console.log(e))
                 })
             } else if (isFolder(fullPath)) {
-                console.log('its a folder');
                 folderReader(fullPath, options).then(fileData => {
-                    resolve(mdlinksFinder(fileData, options));
+                  const pathJoin = path.join(fullPath +'\\'+ fileData);
+                    resolve(mdlinksFinder(pathJoin, options));
                 }).catch(e => console.log(e));
             }
         } catch (error) {
             reject(error);
         }
     });
-}
+};
+// mdlinksFinder('./README.md', { validate: true, stats: true }).then(r => console.log(r)).catch(e => console.log(e));
 
 module.exports = mdlinksFinder;
