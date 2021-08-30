cab23bf6e7f228ed028a72bdd1822e3d111c6b44
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-08-30 12:55:45 -0300

    stats links, read folder inside of a folder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 if (require.main === module) {
     if (process.argv.length > 2) {
         const fullPath = path.resolve(process.argv[2]);
-        mdlinksFinder(fullPath, { validate: process.argv[3] === '--validate' ? true : false }).then(r => console.log(r)).catch(e => console.log(e))
+        mdlinksFinder(fullPath, { validate: process.argv[3] === '--validate' || process.argv[4] === '--validate' ? true : false, stats: process.argv[3] === '--stats' || process.argv[4] === '--stats' ? true : false }).then(r => console.log(r)).catch(e => console.log(e))
     } else {
         console.log('Usage: node . "path to file"', '\n', 'also can add a second param "--validate": to validate the links in the md file')
     }
